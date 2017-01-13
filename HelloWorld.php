37f25ae665d9f21496a7ef488b137e34517fce1a
Mem-Dixy <spammeifyoucan@me.com>
2017-01-13 02:39:01 +0000

    Add files via upload

---
+++ HelloWorld.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!-- S00695336 -->
+<html>
+    <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>
+            My First PHP Application
+        </title>
+    </head>
+    <body>
+        <h3>
+            My First PHP Application
+        </h3><?php
+/*
+ * Name: Clifford Peters
+ * Age: 00011010 
+ */
+echo "Hello World";
+$greeting = "PHP is fun!\n";
+echo $greeting;
+?>
+    </body>
+</html>
