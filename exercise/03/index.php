63638337b61d33ac196838f6c097d8cb4a500bf5
Mem-Dixy <spammeifyoucan@me.com>
2017-01-23 21:24:00 +0000

    CE03
    Friendlyier input form.

--- exercise/03/index.php
+++ exercise/03/index.php
@@ -5,6 +5,8 @@ $class = htmlentities($_POST["class"]);
 $age = htmlentities($_POST["age"]);
 $gender = htmlentities($_POST["gender"]);
 $kingdom = htmlentities($_POST["kingdom"]);
+//$image = "".$race."".$class."".$gender."";
+$image = $race.$class.$gender;
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,16 +15,40 @@ $kingdom = htmlentities($_POST["kingdom"]);
 		<title>
 			Class Exercise 03
 		</title>
+		<link rel="stylesheet" href="style.css" />
 	</head>
 	<body>
+            <?php print(rand()."<br />"); ?>
 		<form action="index.php" method="post">
-			<input name="name" type="text" value="<?php print($name); ?>" /><br />
-			<input name="race" type="text" value="<?php print($race); ?>" /><br />
-			<input name="class" type="text" value="<?php print($class); ?>" /><br />
-			<input name="age" type="text" value="<?php print($age); ?>" /><br />
-			<input name="gender" type="text" value="<?php print($gender); ?>" /><br />
-			<input name="kingdom" type="text" value="<?php print($kingdom); ?>" /><br />
-			<input type="submit" value="Send <?php print(rand()); ?>" />
+			<input name="name" type="text" /><br />
+			<select name="race">
+				<option>Dw</option>
+				<option>El</option>
+				<option>Ha</option>
+				<option>Hu</option>
+			</select><br />
+			<select name="class">
+				<option>Cl</option>
+				<option>Fi</option>
+				<option>Ma</option>
+				<option>Th</option>
+			</select><br />
+			<input name="age" type="number" /><br />
+			<select name="gender">
+				<option>Ma</option>
+				<option>Fe</option>
+			</select><br />
+			<input name="kingdom" type="text" /><br />
+			<input type="submit" value="Send" />
 		</form>
+		<?php
+			print("name=".$name."<br />");
+			print("race=".$race."<br />");
+			print("class=".$class."<br />");
+			print("age=".$age."<br />");
+			print("gender=".$gender."<br />");
+			print("kingdom=".$kingdom."<br />");
+			print("<img alt=\"image\" src=\"image/".$image.".jpg\" />");
+		?>
 	</body>
 </html>
